287a0e1c159aff183ab499f68cd4e46ffca3e777
Daniel Liu <danielliuzy@fb.com>
2023-12-02 14:25:05 -0800

    move assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { StatusRow } from "./StatusRow";
 
 const App = () => {
+  const [animated, setAnimated] = useState(false);
   return (
     <div className="bg-gray-400 w-screen h-screen flex flex-col items-center px-6">
       {/* Top navigation bar */}
@@ -12,9 +14,16 @@ const App = () => {
       <div className="w-full m-4 bg-blue-400 p-6 rounded-lg shadow-lg">
         <div className="flex flex-col items-center">
           <div className="rounded-full p-2">
-            <img className="rounded-lg" src="./src/assets/tamagotchi.png" />
+            <img
+              className="rounded-lg"
+              src={`/tamagotchi.${animated ? "gif" : "png"}`}
+            />
           </div>
           <h1 className="text-2xl font-bold mt-2">Tamagotchi</h1>
+          <div className="items-center flex space-x-1">
+            <input type="checkbox" onChange={() => setAnimated(!animated)} />{" "}
+            <div className="text-sm font-light">Animate</div>
+          </div>
         </div>
       </div>
       <div className="w-full m-4 bg-blue-400 p-6 rounded-lg shadow-lg">
